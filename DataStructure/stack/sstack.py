7dfe8657f69112d09c3e48963af06181c5b9a69a
ZRQ0410 <1292732905@qq.com>
2022-08-21 16:30:50 +0100

    add stack structure

--- DataStructure/stack/sstack.py
+++ DataStructure/stack/sstack.py
@@ -6,6 +6,11 @@
 """
 
 
+# 自定义异常
+class StackError(Exception):
+    pass
+
+
 # 顺序栈类
 class SStack:
     def __init__(self):
@@ -13,6 +18,31 @@ class SStack:
         # 列表最后一个元素作为栈顶
         self._elems = []
 
+    def is_empty(self):
+        return self._elems == []
+
+    # 入栈
+    def push(self, val):
+        self._elems.append(val)
+
+    # 出栈
+    def pop(self):
+        if self.is_empty():
+            raise StackError("Stack is empty")
+        # 弹出并返回
+        return self._elems.pop()
+
+    # 查看栈顶元素
+    def top(self):
+        if self.is_empty():
+            raise StackError("Stack is empty")
+        return self._elems[-1]
+
 
 if __name__ == "__main__":
     st = SStack()
+    st.push(10)
+    st.push(20)
+    st.push(30)
+    while not st.is_empty():
+        print(st.pop())
